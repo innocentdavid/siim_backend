3e8c83b3285e2d021a6e9e09d04d6e6dda788b0d
innocentdavid <paulinnocent04@gmail.com>
2023-02-02 09:44:32 +0100

    sds;l

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const chargebee = require("chargebee")
 // CORS is enabled only for demo. Please dont use this in production unless you know about CORS
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+// const { unbilled_charge } = require('chargebee/lib/resources/api_endpoints');
 const PORT = process.env.PORT || 8000
 
 chargebee.configure({
   site: "sproutysocial",
-  api_key: "live_JtEKTrE7pAsvrOJar1Oc8zhdk5IbvWzE"
-  // api_key: "live_BW3FVqcdbW4naokDniIcdajdNBWm3MJc1v"
+  // api_key: "live_JtEKTrE7pAsvrOJar1Oc8zhdk5IbvWzE"
+  api_key: "live_BW3FVqcdbW4naokDniIcdajdNBWm3MJc1v"
 });
 const app = express()
 
@@ -16,11 +17,11 @@ app.use(express.urlencoded())
 // app.use(cors(corsOptions))
 app.use(cors())
 
-function addDays(days) {
-  var result = new Date();
-  result.setDate(result.getDate() + days);
-  return result;
-}
+// function addDays(days) {
+//   var result = new Date();
+//   result.setDate(result.getDate() + days);
+//   return result;
+// }
 
 app.post("/api/generate_checkout_new_url", (req, res) => {
   chargebee.hosted_page.checkout_new_for_items({
@@ -37,7 +38,92 @@ app.post("/api/generate_checkout_new_url", (req, res) => {
       console.log(error);
       // res.send(error);
     } else {
+      res.send(result.hosted_page);
+    }
+  });
+});
+
+app.post("/api/cancel_for_items", (req, res) => {
+  chargebee.subscription.cancel_for_items(req.body.subscription_item_id, {
+    end_of_term: false
+  }).request(function (error, result) {
+    if (error) {
+      //handle error
+      console.log(error);
+    } else {
+      // console.log(result);
+      var subscription = result.subscription;
+      var customer = result.customer;
+      var card = result.card;
+      var invoice = result.invoice;
+      var unbilled_charges = result.unbilled_charges;
+      var credit_notes = result.credit_notes;
+      res.send(card, invoice, credit_notes, subscription, customer, unbilled_charges)
+    }
+  });
+});
+
+app.post("/api/customer_list", (req, res) => {
+  chargebee.customer.list({
+    limit: 1,
+    "email[is]": req.body.email
+  }).request(function (error, result) {
+    if (error) {
+      //handle error
+      console.log(error);
+    } else {
+      var customer = {};
+      for (var i = 0; i < result.list.length; i++) {
+        var entry = result.list[i]
+        // console.log(entry);
+        customer = entry.customer;
+        // var card = entry.card;
+      }
+      // console.log(customer);
       console.log(result);
+      // const customer = result.customer
+      res.send(customer)
+    }
+  });
+});
+
+app.post("/api/subscription_list", (req, res) => {
+  chargebee.subscription.list({
+    limit: 1,
+    "customer_id[is]": req.body.customer_id,
+}).request(function (error, result) {
+    if (error) {
+      //handle error
+      console.log(error);
+    } else {
+      var customer_subscription = {};
+      // console.log(result?.list?.[0]?.subscription);
+      for (var i = 0; i < result.list.length; i++) {
+        var entry = result.list[i]
+        customer_subscription = entry.subscription;
+      }
+      // console.log(customer_subscription);
+      res.send(customer_subscription)
+    }
+  });
+});
+
+app.post("/api/pre_cancel", (req, res) => {
+  chargebee.hosted_page.pre_cancel({
+    // pass_thru_content: "{custom :  {discount_percent: 10}}",
+    subscription: {
+      // id: "__test__KyVnHhSBWmCoF2tJ"
+      // id: "__live__B8NglNTUdZxSWoD"
+      id: "B8NglNTUdZxSWoD"
+    },
+    // cancel_url: 'https//sprouty-social.vercel.app'
+  }).request(function (error, result) {
+    if (error) {
+      //handle error
+      console.log(error);
+      // res.send(error);
+    } else {
+      // console.log(result);
       res.send(result.hosted_page);
     }
   });
@@ -74,7 +160,6 @@ app.post("/api/generate_update_payment_method_url", (req, res) => {
   });
 });
 
-
 app.post("/api/generate_portal_session", (req, res) => {
   chargebee.portal_session.create({
     customer: {
@@ -104,5 +189,5 @@ app.post('/api/generate_payment_intent', (req, res) => {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// app.listen(8000, () => console.log('Example app listening on port 8000!'))
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.listen(8000, () => console.log('Example app listening on port 8000!'))
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
